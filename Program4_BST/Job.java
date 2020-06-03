573d0d6cf0c2e0fa8f6147164f578f5b4524eee5
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2020-06-02 22:48:53 -0700

    Create Job.java

---
+++ Program4_BST/Job.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Job extends Source {
+    protected String a_date;
+    protected String a_time;
+    protected float a_wage;
+
+    /**
+     * Default constructor
+     */
+    public Job() {
+        super();
+        this.a_date = new String();
+        this.a_time = new String();
+        this.a_wage = 0.0f;
+    }
+
+    /**
+     * Copy Constructor.
+     * @param in_Job
+     */
+    public Job(Job in_Job) {
+        super(in_Job);
+        this.a_date = in_Job.a_date;
+        this.a_time = in_Job.a_time;
+        this.a_wage = in_Job.a_wage;
+    }
+
+    /**
+     * These functions don't do anything.
+     * I needed it here in order to satisfy the syntax checker's
+     * requirements for dynamic binding.
+     */
+    public boolean ReadFromUser() { return true; }
+    public int OutPut() {
+        return 1;
+    }
+    public boolean ReadFromFile(Scanner inSC, String tempType) throws FileNotFoundException { return true; }
+}
+
