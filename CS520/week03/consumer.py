40b042d01a67b00836dea70b0b78cc35081cd9e2
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-04-21 22:45:15 -0700

    Create consumer.py

---
+++ CS520/week03/consumer.py
@@ -0,0 +1,36 @@
+                                                                                                                     X#!/usr/bin/env python
+
+import sys
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    config.update(config_parser['consumer'])
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(consumer, partitions):
+        if args.reset:
+            for p in partitions:
+                p.offset = OFFSET_BEGINNING
+            consumer.assign(partitions)
+
+    # Subscribe to topic
+    topic = "purchases"
+    consumer.subscribe([topic], on_assign=reset_offset)
+                                                                                                                                1,1           Top
